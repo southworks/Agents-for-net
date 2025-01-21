0787e7f428c8a0cad40cd69cfd31c86c08d7d417
Ajay Jadhav <v-ajayjadhav@microsoft.com>
2025-01-21 18:34:22 +0530

    Adding Meeting-Notification-Sample

---
+++ src/samples/Teams/Meetings-Notification/Models/SubmitFeedback.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InMeetingNotificationsBot.Models
+{
+    public class SubmitFeedbackAction : PushAgendaAction
+    {
+        public string Topic { get; set; }
+        public string Feedback { get; set; }
+
+        public int Choice { get; set; }
+    }
+}
