ec2c9a060b7bfa0e41d6640ef13a9c9b556cb82f
Mohammed Furquan <v-mfurquan@microsoft.com>
2025-01-22 08:36:49 +0530

    Added Bot Tag Mention Sample To Copilot Agent Repo

---
+++ src/samples/Teams/bot-tag-mention/Bots/TeamsConversationBot.cs
@@ -0,0 +1,63 @@
+﻿// <copyright file="TeamsConversationBot.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Agents.BotBuilder.Dialogs;
+using Microsoft.Agents.State;
+using Microsoft.Agents.Core.Interfaces;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Extensions.Logging;
+
+namespace TagMentionBot.Bots
+{
+    public class TeamsConversationBot<T> : DialogBot<T> where T : Dialog
+    {
+        private string _appId;
+        private string _appPassword;
+
+        public TeamsConversationBot(ConversationState conversationState, UserState userState, T dialog, ILogger<DialogBot<T>> logger)
+                    : base(conversationState, userState, dialog, logger)
+        {
+        }
+
+        /// <summary>
+        /// Called when a new user is joins the team, or when the application is first installed
+        /// </summary>
+        /// <param name="membersAdded"></param>
+        /// <param name="teamInfo"></param>
+        /// <param name="turnContext"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var teamMember in membersAdded)
+            {
+                if (teamMember.Id != turnContext.Activity.Recipient.Id && turnContext.Activity.Conversation.ConversationType != "personal")
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to the team {teamMember.Name}"), cancellationToken);
+                }
+            }
+        }
+
+        /// <summary>
+        /// This is triggered during the app installation update activity
+        /// </summary>
+        /// <param name="turnContext"></param>
+        /// <param name="cancellationToken"></param>
+        /// <returns>A task that represents the work queued to execute.</returns>
+        protected override async Task OnInstallationUpdateActivityAsync(ITurnContext<IInstallationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Conversation.ConversationType == "channel")
+            {
+                await turnContext.SendActivityAsync($"Welcome to Tag mention Teams bot app. Please follow the below commands for mentioning the tags: \r\n\r\n\r\n1. Command: \"`@<Bot-name> <your-tag-name>`\" - It will work only if you have Graph API permissions to fetch the tags and bot will mention the tag accordingly in team's channel scope.\r\n\r\n\r\n2. Command \"`@<Bot-name> @<your-tag>`\" - It will work without Graph API permissions but you need to provide the tag as command to experience tag mention using bot.");
+            }
+            else
+            {
+                await turnContext.SendActivityAsync("Welcome to Tag mention demo bot. Type anything to get logged in. Type 'logout' to sign-out");
+            }
+        }
+    }
+}
---
+++ src/samples/Teams/bot-tag-mention/Program.cs
@@ -0,0 +1,56 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using TagMentionBot;
+using TagMentionBot.Bots;
+using TagMentionBot.Dialogs;
+using Microsoft.Agents.State;
+using Microsoft.Agents.Hosting.AspNetCore;
+using Microsoft.Agents.Storage;
+using Microsoft.Agents.Samples;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddHttpClient();
+
+builder.Logging.AddConsole();
+builder.Logging.AddDebug();
+
+// Add AspNet token validation
+builder.Services.AddBotAspNetAuthentication(builder.Configuration);
+
+// Add basic bot functionality
+builder.AddBot<TeamsConversationBot<MainDialog>, TeamsSSOAdapter>();
+
+// Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+// Create the User state. (Used in this bot's Dialog implementation.)
+builder.Services.AddSingleton<UserState>();
+
+// Create the Conversation state. (Used by the Dialog system itself.)
+builder.Services.AddSingleton<ConversationState>();
+
+// The Dialog that will be run by the bot.
+builder.Services.AddSingleton<MainDialog>();
+
+var app = builder.Build();
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/", () => "Microsoft Copilot SDK Sample");
+    app.UseDeveloperExceptionPage();
+    app.MapControllers().AllowAnonymous();
+}
+else
+{
+    app.MapControllers();
+}
+
+app.Run();
