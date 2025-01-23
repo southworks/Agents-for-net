e09fb568549bec5b893d89f7dd8b019e9cf16171
CeciliaAvila <cecilia.avila@southworks.com>
2025-01-23 09:35:55 -0300

    Add tests for Authentication.Msal Model

---
+++ src/tests/Microsoft.Agents.Authentication.Msal.Tests/Model/ConnectionSettingsTests.cs
@@ -0,0 +1,177 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.Authentication.Msal.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Agents.Authentication.Msal.Tests.Model
+{
+    public class ConnectionSettingsTests
+    {
+        private Dictionary<string, string> configSettings;
+        private const string SettingsSection = "Connections:Settings";
+
+        public ConnectionSettingsTests()
+        {
+            configSettings = new Dictionary<string, string> {
+                { "Connections:Settings:ClientId", "test-client-id" },
+                { "Connections:Settings:ClientSecret", "test-client-secret" },
+                { "Connections:Settings:CertificateThumbPrint", "test-thumbprint" },
+                { "Connections:Settings:CertificateSubjectName", "test-subject-name" },
+                { "Connections:Settings:Authority", "test-authority" },
+                { "Connections:Settings:TenantId", "test-tenant-id" },
+            };            
+        }
+
+        [Fact]
+        public void ConnectionSettings_ShouldDefaultToClientSecretType()
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            var settings = new ConnectionSettings(configuration.GetSection(SettingsSection));
+
+            Assert.Equal(AuthTypes.ClientSecret, settings.AuthType);
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullClientIdForCertificateType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "Certificate");
+            configSettings.Remove("Connections:Settings:ClientId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullCertificateThumbPrintForCertificateType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "Certificate");
+            configSettings.Remove("Connections:Settings:CertificateThumbPrint");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullAuthorityForCertificateType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "Certificate");
+            configSettings.Remove("Connections:Settings:Authority");
+            configSettings.Remove("Connections:Settings:TenantId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullClientIdForCertificateSubjectNameType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "CertificateSubjectName");
+            configSettings.Remove("Connections:Settings:ClientId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullCertificateSubjectNameForCertificateSubjectNameType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "CertificateSubjectName");
+            configSettings.Remove("Connections:Settings:CertificateSubjectName");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullAuthorityForCertificateSubjectNameType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "CertificateSubjectName");
+            configSettings.Remove("Connections:Settings:Authority");
+            configSettings.Remove("Connections:Settings:TenantId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullClientIdForClientSecretType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "ClientSecret");
+            configSettings.Remove("Connections:Settings:ClientId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullClientSecretForClientSecretType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "ClientSecret");
+            configSettings.Remove("Connections:Settings:ClientSecret");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullAuthorityForClientSecretType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "ClientSecret");
+            configSettings.Remove("Connections:Settings:Authority");
+            configSettings.Remove("Connections:Settings:TenantId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+
+        [Fact]
+        public void ValidateConfiguration_ShouldThrowOnNullClientIdForUserManagedIdentityType()
+        {
+            configSettings.Add("Connections:Settings:AuthType", "UserManagedIdentity");
+            configSettings.Remove("Connections:Settings:ClientId");
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            Assert.Throws<ArgumentNullException>(() => new ConnectionSettings(configuration.GetSection(SettingsSection)));
+        }
+    }
+}
---
+++ src/tests/Microsoft.Agents.Authentication.Msal.Tests/Model/MsalAuthConfigurationOptionsTests.cs
@@ -0,0 +1,66 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.Authentication.Msal.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.Agents.Authentication.Msal.Tests.Model
+{
+    public class MsalAuthConfigurationOptionsTests
+    {
+        [Fact]
+        public void CreateFromConfigurationOptions_ShouldReturnDefaultOptionsOnNullConfig()
+        {
+            var options = MsalAuthConfigurationOptions.CreateFromConfigurationOptions(null);
+
+            Assert.NotNull(options);
+            Assert.False(options.MSALEnabledLogPII);
+            Assert.Equal(3, options.MSALRetryCount);
+            Assert.Equal(new TimeSpan(0, 0, 0, 30), options.MSALRequestTimeout);
+        }
+
+        [Fact]
+        public void CreateFromConfigurationOptions_ShouldReturnCustomOptionsFromConfig()
+        {
+            var configSettings = new Dictionary<string, string> {
+                { "MsalOptions:MSALEnabledLogPII", "true" },
+                { "MsalOptions:MSALRetryCount", "5" },
+                { "MsalOptions:MSALRequestTimeout", "00:00:01:00" },
+            };
+            var SettingsSection = "MsalOptions";
+
+            IConfiguration configuration = new ConfigurationBuilder()
+                .AddInMemoryCollection(configSettings)
+                .Build();
+
+            var options = MsalAuthConfigurationOptions.CreateFromConfigurationOptions(configuration.GetSection(SettingsSection));
+
+            Assert.NotNull(options);
+            Assert.True(options.MSALEnabledLogPII);
+            Assert.Equal(5, options.MSALRetryCount);
+            Assert.Equal(new TimeSpan(0, 0, 1, 00), options.MSALRequestTimeout);
+        }
+
+        [Fact]
+        public void UpdateOptions_ShouldUpdateDeaultValues()
+        {
+            var options = MsalAuthConfigurationOptions.CreateFromConfigurationOptions(null);
+
+            var updatedOptions = new MsalAuthConfigurationOptions()
+            {
+                MSALEnabledLogPII = true,
+                MSALRetryCount = 5,
+                MSALRequestTimeout = TimeSpan.FromSeconds(1),
+            };
+
+            options.UpdateOptions(updatedOptions);
+
+            Assert.Equal(updatedOptions.MSALEnabledLogPII, options.MSALEnabledLogPII);
+            Assert.Equal(updatedOptions.MSALRetryCount, options.MSALRetryCount);
+            Assert.Equal(updatedOptions.MSALRequestTimeout, options.MSALRequestTimeout);
+        }
+    }
+}
