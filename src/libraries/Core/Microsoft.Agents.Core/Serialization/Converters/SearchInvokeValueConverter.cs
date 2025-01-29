73bdbf28091eb3ed50123665cc786f8a3b6b2b7f
Tracy Boehrer <trboehre@microsoft.com>
2025-01-28 18:22:02 -0600

    Corrected SearchInvokeValue.QueryOptions type

---
+++ src/libraries/Core/Microsoft.Agents.Core/Serialization/Converters/SearchInvokeValueConverter.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.Core.Models;
+using System.Text.Json;
+
+namespace Microsoft.Agents.Core.Serialization.Converters
+{
+    internal class SearchInvokeValueConverter : ConnectorConverter<SearchInvokeValue>
+    {
+        /// <inheritdoc/>
+        protected override bool TryReadGenericProperty(ref Utf8JsonReader reader, SearchInvokeValue value, string propertyName, JsonSerializerOptions options)
+        {
+            if (propertyName.Equals(nameof(value.Context)))
+            {
+                SetGenericProperty(ref reader, data => value.Context = data, options);
+            }
+            else
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
