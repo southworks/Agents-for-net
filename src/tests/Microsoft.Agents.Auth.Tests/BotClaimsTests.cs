09357c2623de698906525d9352a340b293a6a402
CeciliaAvila <cecilia.avila@southworks.com>
2025-01-31 11:25:11 -0300

    Add tests for BotClaims class

--- src/tests/Microsoft.Agents.Auth.Tests/BotClaimsTests.cs
+++ src/tests/Microsoft.Agents.Auth.Tests/BotClaimsTests.cs
@@ -206,14 +206,14 @@ namespace Microsoft.Agents.Auth.Tests
         [Fact]
         public void GetOutgoingAppId_ValidV2VersionClaimWithAuthorizedParty()
         {
-            // this tests sets up a claimset with a version claim "2.0" and an AuthorizedParty Claim
-            var claims = new List<Claim>
+            static IEnumerable<Claim> GetClaims() // Use this function to cover the ToList() method.
             {
-                new(AuthenticationConstants.VersionClaim, "2.0"),
-                new(AuthenticationConstants.AuthorizedParty, "appId") 
+                // this tests sets up a claimset with a version claim "2.0" and an AuthorizedParty Claim.
+                yield return new Claim(AuthenticationConstants.VersionClaim, "2.0");
+                yield return new Claim(AuthenticationConstants.AuthorizedParty, "appId");
             };
 
-            Assert.Equal("appId", BotClaims.GetOutgoingAppId(claims));
+            Assert.Equal("appId", BotClaims.GetOutgoingAppId(GetClaims()));
         }
 
         [Fact]
@@ -264,6 +264,46 @@ namespace Microsoft.Agents.Auth.Tests
             };
 
             Assert.False(BotClaims.IsBotClaim(claims));
+        }
+
+        [Fact]
+        public void IsBotClaim_ShouldReturnFalseOnNoVersion()
+        {
+            var claims = new List<Claim>
+            {
+                new(AuthenticationConstants.AudienceClaim, "audience"),
+                new(AuthenticationConstants.AppIdClaim, "appId")
+            };
+
+            Assert.False(BotClaims.IsBotClaim(claims));
+        }
+
+        [Fact]
+        public void IsBotClaim_ShouldReturnFalseOnNoOutgoingAppId()
+        {
+            // Setup an invalid claim set
+            var claims = new List<Claim>
+            {
+                new(AuthenticationConstants.VersionClaim, "2.0"),
+                new(AuthenticationConstants.AudienceClaim, "audience"),
+                new(AuthenticationConstants.AppIdClaim, "appId") // not a valid claim on a v2 token
+            };
+
+            Assert.False(BotClaims.IsBotClaim(claims));
+        }
+
+        [Fact]
+        public void GetTokenAudience_ShouldSetAudienceInUri()
+        {
+            var claims = new List<Claim>
+            {
+                new(AuthenticationConstants.VersionClaim, "2.0"),
+                new(AuthenticationConstants.AuthorizedParty, "appId")
+            };
+
+            var audience = BotClaims.GetTokenAudience(claims);
+
+            Assert.Equal("app://appId", audience);
         }
     }
 }
