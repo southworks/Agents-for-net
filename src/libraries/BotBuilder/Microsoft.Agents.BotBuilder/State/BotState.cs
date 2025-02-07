9a1bc0c412cf5a56dc5fb0bb22329620594d8e6a
Tracy Boehrer <trboehre@microsoft.com>
2025-02-07 10:18:06 -0600

    Corrected IStatePropertyAccessor.GetValue for back-compat

--- src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/State/BotState.cs
+++ src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/State/BotState.cs
@@ -477,34 +477,11 @@ namespace Microsoft.Agents.BotBuilder.State
             /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
             public async Task<T> GetAsync(ITurnContext turnContext, Func<T> defaultValueFactory, CancellationToken cancellationToken)
             {
-                T result = default(T);
-
                 await _botState.LoadAsync(turnContext, false, cancellationToken).ConfigureAwait(false);
 
-                try
-                {
-                    // if T is a value type, lookup up will throw key not found if not found, but as perf
-                    // optimization it will return null if not found for types which are not value types (string and object).
-                    result = _botState.GetValue<T>(Name, defaultValueFactory);
-
-                    if (result == null && defaultValueFactory != null)
-                    {
-                        // use default Value Factory and save default value for any further calls
-                        result = defaultValueFactory();
-                        await SetAsync(turnContext, result, cancellationToken).ConfigureAwait(false);
-                    }
-                }
-                catch (KeyNotFoundException)
-                {
-                    if (defaultValueFactory != null)
-                    {
-                        // use default Value Factory and save default value for any further calls
-                        result = defaultValueFactory();
-                        await SetAsync(turnContext, result, cancellationToken).ConfigureAwait(false);
-                    }
-                }
-
-                return result;
+                // if T is a value type, lookup up will throw key not found if not found, but as perf
+                // optimization it will return null if not found for types which are not value types (string and object).
+                return _botState.GetValue<T>(Name, defaultValueFactory);
             }
 
             /// <summary>
