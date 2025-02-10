e2666d3b0629cbcc8d0920103be9e8dba26f6d3c
Tracy Boehrer <trboehre@microsoft.com>
2025-02-10 16:18:49 -0600

    MessagingExtensionActionResponse roundtrip serialize tests

--- src/tests/Microsoft.Agents.Extensions.Teams.Tests/Model/MessagingExtensionActionResponseTests.cs
+++ src/tests/Microsoft.Agents.Extensions.Teams.Tests/Model/MessagingExtensionActionResponseTests.cs
@@ -1,9 +1,13 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Collections.Generic;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Agents.Core.Serialization;
 using Microsoft.Agents.Extensions.Teams.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using Xunit;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace Microsoft.Agents.Extensions.Teams.Tests.Model
 {
@@ -36,5 +40,48 @@ namespace Microsoft.Agents.Extensions.Teams.Tests.Model
             Assert.NotNull(msgExtActionResponse);
             Assert.IsType<MessagingExtensionActionResponse>(msgExtActionResponse);
         }
+
+        [Fact]
+        public void MessagingExtensionActionResponseRoundTrip()
+        {
+            var task = new TaskModuleResponseBase("message") { Properties = ProtocolJsonSerializer.ToJsonElements(new { prop1 = "prop1" }) };
+            var composeExtension = new MessagingExtensionResult()
+            {
+                AttachmentLayout = "attachmentLayout",
+                Type = "type",
+                Attachments = [new MessagingExtensionAttachment() { }],
+                SuggestedActions = new MessagingExtensionSuggestedAction() { Actions = [new CardAction() 
+                    {
+                        Type = "type",
+                        Title = "title",
+                        Image = "image",
+                        ImageAltText = "imageAltText",
+                        Text = "text",
+                        DisplayText = "displayText",
+                        Value = new { value = "value" },
+                        ChannelData = new { channelData = "channelData"}
+                    }] },
+                Text = "text",
+                ActivityPreview = new Activity()
+            };
+    
+            var msgExtActionResponse = new MessagingExtensionActionResponse(task, composeExtension)
+            {
+                CacheInfo = new CacheInfo() { CacheDuration = 1, CacheType = "cacheType"}
+            };
+
+            // Known good
+            var goodJson = LoadTestJson.LoadJson(msgExtActionResponse);
+
+            // Out
+            var json = ProtocolJsonSerializer.ToJson(msgExtActionResponse);
+            Assert.Equal(goodJson, json);
+
+            // In
+            var inObj = ProtocolJsonSerializer.ToObject<MessagingExtensionActionResponse>(json);
+            json = ProtocolJsonSerializer.ToJson(inObj);
+            Assert.Equal(goodJson, json);
+        }
+
     }
 }
