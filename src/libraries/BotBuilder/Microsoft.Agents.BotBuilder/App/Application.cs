1c178f1c5b10536376d29ac1b0e990b707a9abea
Tracy Boehrer <trboehre@microsoft.com>
2025-02-13 10:03:49 -0600

    Added Extensions back to solution lost during merge

--- src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/App/Application.cs
+++ src/libraries/BotBuilder/Microsoft.Agents.BotBuilder/App/Application.cs
@@ -7,7 +7,6 @@ using Microsoft.Agents.Core.Models;
 using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
 using System.Threading.Tasks;
