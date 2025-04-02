0c406129e1b5b450898d1d594a25299f9aea9491
Tracy Boehrer <trboehre@microsoft.com>
2025-04-02 15:59:28 -0500

    Remvoved unused `using`

--- src/libraries/Core/Microsoft.Agents.Core/Models/Channels.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Models/Channels.cs
@@ -1,8 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-using System.Diagnostics;
-
 namespace Microsoft.Agents.Core.Models
 {
     public static class Channels
