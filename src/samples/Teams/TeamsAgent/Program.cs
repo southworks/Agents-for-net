9400b778bcdbeba6a8d1cdeffe9a74c13cb5be2c
Ricardo Minguez Pablos (RIDO) <rmpablos@microsoft.com>
2025-04-25 16:16:26 -0700

    Return Card on Selected Item

--- src/samples/Teams/TeamsAgent/Program.cs
+++ src/samples/Teams/TeamsAgent/Program.cs
@@ -2,26 +2,16 @@ using Microsoft.Agents.Builder;
 using Microsoft.Agents.Hosting.AspNetCore;
 using Microsoft.Agents.Samples;
 using Microsoft.Agents.Storage;
-using TeamsAgent;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
 builder.Services.AddHttpClient();
-builder.Logging.AddConsole();
-
 builder.Services.AddAgentAspNetAuthentication(builder.Configuration);
-
 builder.AddAgentApplicationOptions();
-
-builder.AddAgent<MyAgent>();
-
+builder.AddAgent<TeamsAgent.TeamsAgent>();
 builder.Services.AddSingleton<IStorage, MemoryStorage>();
-
 var app = builder.Build();
 
-app.MapPost("/api/messages", async (HttpRequest request, HttpResponse response, IAgentHttpAdapter adapter, IAgent agent, CancellationToken cancellationToken) =>
-{
-    await adapter.ProcessAsync(request, response, agent, cancellationToken);
-});
+app.MapPost("/api/messages", 
+    (HttpRequest request, HttpResponse response, IAgentHttpAdapter adapter, IAgent agent, CancellationToken cancellationToken) =>
+        adapter.ProcessAsync(request, response, agent, cancellationToken));
 app.Run();
---
+++ src/samples/Teams/TeamsAgent/TeamsAgent.cs
@@ -0,0 +1,172 @@
+﻿using AdaptiveCards.Rendering;
+using Microsoft.Agents.Builder;
+using Microsoft.Agents.Builder.App;
+using Microsoft.Agents.Builder.App.AdaptiveCards;
+using Microsoft.Agents.Builder.State;
+using Microsoft.Agents.Core.Models;
+using Microsoft.Agents.Extensions.Teams.App;
+using Microsoft.Agents.Extensions.Teams.Connector;
+using Microsoft.Agents.Extensions.Teams.Models;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace TeamsAgent
+{
+    public class TeamsAgent : AgentApplication
+    {
+        IHttpClientFactory _httpClientFactory;
+        public TeamsAgent(AgentApplicationOptions options, IHttpClientFactory httpClientFactory) : base(options)
+        {
+            _httpClientFactory = httpClientFactory;
+            RegisterExtension(new TeamsAgentExtension(this), tae =>
+            {
+                tae.OnMessageEdit(MessageEdited);
+                tae.MessageExtensions.OnQuery("findNuGetPackage", OnQuery);
+                tae.MessageExtensions.OnSelectItem(OnSelectItem);
+            });
+            OnMessage("/help", (t, _, ct) => t.SendActivityAsync("TeamsAgent demo", cancellationToken: ct));
+            OnMessageReactionsAdded(OnMessageReaction);
+            OnConversationUpdate(ConversationUpdateEvents.MembersAdded, WelcomeMessageAsync);
+            OnActivity(ActivityTypes.Message, OnMessageAsync);
+        }
+
+        private async Task<MessagingExtensionResult> OnSelectItem(ITurnContext turnContext, ITurnState turnState, object item, CancellationToken cancellationToken)
+        {
+            var package = JsonSerializer.Deserialize<PackageItem>((JsonElement)item);
+            if (package is null)
+            {
+                await turnContext.SendActivityAsync("selected item is not a packageItem", cancellationToken: cancellationToken);
+                return null!;
+            }
+
+            await turnContext.SendActivityAsync("selected item " + JsonSerializer.Serialize(item), cancellationToken: cancellationToken);
+            var card = new ThumbnailCard
+            {
+                Title = $"{package.PackageId}, {package.Version}",
+                Subtitle = package.Description,
+                Buttons = new List<CardAction>
+                    {
+                        new CardAction { Type = ActionTypes.OpenUrl, Title = "Nuget Package", Value = $"https://www.nuget.org/packages/{package.PackageId}" },
+                        new CardAction { Type = ActionTypes.OpenUrl, Title = "Project", Value = package.ProjectUrl},
+                    },
+            };
+
+            if (!string.IsNullOrEmpty(package.IconUrl))
+            {
+                card.Images = new List<CardImage>() { new CardImage(package.IconUrl, "Icon") };
+            }
+
+            var attachment = new MessagingExtensionAttachment
+            {
+                ContentType = ThumbnailCard.ContentType,
+                Content = card,
+            };
+
+            return await Task.FromResult(new MessagingExtensionResult
+            {
+                Type = "result",
+                AttachmentLayout = "list",
+                Attachments = new List<MessagingExtensionAttachment> { attachment }
+            });
+        }
+
+        private async Task<MessagingExtensionResult> OnQuery(ITurnContext turnContext, ITurnState turnState, Query<IDictionary<string, object>> query, CancellationToken cancellationToken)
+        {
+
+            JsonElement el = query.Parameters.TryGetValue<JsonElement>("NuGetPackageName");
+
+            string text = el.GetString() ?? string.Empty;
+
+
+            IEnumerable<PackageItem> packages = await FindPackages(text);
+            List<MessagingExtensionAttachment> attachments = [.. packages.Select(package =>
+            {
+                string cardValue = "{{\"id\": \"{package.PackageId}\", \"version\": \"{package.Version}\", \"description\": \"{PackageItem.NormalizeString(package.Description!)}\", \"projectUrl\": \"{package.ProjectUrl}\", \"iconUrl\": \"{package.IconUrl}\"}}";
+
+                ThumbnailCard previewCard = new() { Title = package.PackageId, Tap = new CardAction { Type = "invoke", Value = cardValue } };
+                if (!string.IsNullOrEmpty(package.IconUrl))
+                {
+                    previewCard.Images = [new CardImage(package.IconUrl, "Icon")];
+                }
+
+                MessagingExtensionAttachment attachment = new()
+                {
+                    ContentType = HeroCard.ContentType,
+                    Content = new HeroCard { Title = package.Id },
+                    Preview = previewCard.ToAttachment()
+                };
+
+                return attachment;
+            })];
+
+            return new MessagingExtensionResult
+            {
+                Type = "result",
+                AttachmentLayout = "list",
+                Attachments = attachments
+
+            };
+        }
+
+        private async Task<IEnumerable<PackageItem>> FindPackages(string text)
+        {
+            var httpClient = _httpClientFactory.CreateClient();
+            var jsonResult = await new HttpClient().GetStringAsync($"https://azuresearch-usnc.nuget.org/query?q=id:{text}&prerelease=true");
+            var data = JsonDocument.Parse(jsonResult).RootElement.GetProperty("data");
+            var packages = data.Deserialize<PackageItem[]>();
+            return packages!;
+        }
+
+        private Task OnMessageReaction(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken) => 
+            turnContext.SendActivityAsync("Message Reaction: " + turnContext.Activity.ReactionsAdded[0].Type, cancellationToken: cancellationToken);
+
+        private  Task MessageEdited(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken) => 
+            turnContext.SendActivityAsync("Message Edited: " + turnContext.Activity.Id, cancellationToken: cancellationToken);
+
+        private Task WelcomeMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken) => 
+            turnContext.SendActivityAsync(MessageFactory.Text("Welcome to the TeamsAgent sample!"), cancellationToken);
+
+        private async Task OnMessageAsync(ITurnContext turnContext, ITurnState turnState, CancellationToken cancellationToken)
+        {
+            TeamsChannelAccount member = await TeamsInfo.GetMemberAsync(turnContext, turnContext.Activity.From.Id, cancellationToken);
+            string msg = member.Name?? "not teams user";
+            var resp = await turnContext.SendActivityAsync($"hi {msg}, use the '+' option on Teams message textbox to start the MessageExtension search", cancellationToken: cancellationToken);
+        }
+    }
+
+    class Root
+    {
+        [JsonPropertyName("data")]
+        public PackageItem[]? Data { get; set; }
+    }
+
+    class PackageItem
+    {
+        [JsonPropertyName("@id")]
+        public string? Id { get; set; }
+
+        [JsonPropertyName("id")]
+        public string? PackageId { get; set; }
+
+        [JsonPropertyName("version")]
+        public string? Version { get; set; }
+
+        [JsonPropertyName("description")]
+        public string? Description { get; set; }
+
+        [JsonPropertyName("projectUrl")]
+        public string? ProjectUrl { get; set; }
+
+        [JsonPropertyName("iconUrl")]
+        public string? IconUrl { get; set; }
+        public static string NormalizeString(string value)
+        {
+            return value
+                .Replace("\r\n", " ")
+                .Replace("\r", " ")
+                .Replace("\n", " ")
+                .Replace("\"", "\\\"");
+        }
+
+    }
+}
