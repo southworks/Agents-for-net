205f8084430d6522cc614eb3f22c286b71915824
Tracy Boehrer <trboehre@microsoft.com>
2025-04-30 08:38:14 -0500

    Added UnpackObjectStrings unit test

---
+++ src/tests/Microsoft.Agents.Model.Tests/UnpackObjectStringsTests.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.Core.Models;
+using Microsoft.Agents.Core.Serialization;
+using System.Text.Json;
+using Xunit;
+
+namespace Microsoft.Agents.Model.Tests
+{
+    public class UnpackObjectStringsTests
+    {
+        [Fact]
+        public void ObjectWithEscapedStrings()
+        {
+            ProtocolJsonSerializer.SerializationOptions.Encoder = System.Text.Encodings.Web.JavaScriptEncoder.UnsafeRelaxedJsonEscaping;
+            ProtocolJsonSerializer.UnpackObjectStrings = false;
+
+            var incoming = "{\"channelData\":{\"name\":\"myName\"},\"value\":\"{\\\"a\\\": \\\"b\\\"}\"}";
+            var a = ProtocolJsonSerializer.ToObject<Activity>(incoming);
+            Assert.True(a.Value is string);
+            Assert.True(a.ChannelData is JsonElement);
+
+            var outgoing = ProtocolJsonSerializer.ToJson(a);
+
+            var parsedA = JsonSerializer.Deserialize<JsonElement>(incoming);
+            var parsedB = JsonSerializer.Deserialize<JsonElement>(outgoing);
+            var rawA = parsedA.GetRawText();
+            var rawB = parsedB.GetRawText();
+
+            var outActivity = new Activity() { ChannelData = new MyClass() { Name = "myName" }, Value = "\"{\\\"a\\\": \\\"b\\\"}\"" };
+            var inJson = ProtocolJsonSerializer.ToJson(outActivity);
+            var inActivity = ProtocolJsonSerializer.ToObject<IActivity>(inJson);
+
+            Assert.True(rawA == rawB && rawA == inJson && rawA == JsonSerializer.Deserialize<JsonElement>(inJson).GetRawText());
+        }
+    }
+
+    class MyClass
+    {
+        public string Name { get; set; }
+    }
+}
