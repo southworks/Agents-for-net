ae03b8f4789eea7093537ace91bac81754d66ce8
Tracy Boehrer <trboehre@microsoft.com>
2025-04-29 19:42:16 -0500

    WeatherAgent sync

--- src/samples/SemanticKernel/WeatherAgent/Plugins/WeatherForecastPlugin.cs
+++ src/samples/SemanticKernel/WeatherAgent/Plugins/WeatherForecastPlugin.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.SemanticKernel;
-// Copyright (c) Microsoft Corporation. All rights reserved.
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
 using System;
+using Microsoft.Agents.Builder;
+using Microsoft.SemanticKernel;
 using System.Threading.Tasks;
 
 namespace WeatherAgent.Plugins;
 
-public class WeatherForecastPlugin
+public class WeatherForecastPlugin(ITurnContext turnContext)
 {
+
     /// <summary>
     /// Retrieve the weather forecast for a specific date. This is a placeholder for a real implementation
     /// and currently only returns a random temperature. This would typically call a weather service API.
@@ -19,6 +21,14 @@ public class WeatherForecastPlugin
     [KernelFunction]
     public Task<WeatherForecast> GetForecastForDate(string date,  string location)
     {
+        string searchingForDate = date;
+        if ( DateTime.TryParse(date, out DateTime searchingDate) )
+        {
+            searchingForDate = searchingDate.ToLongDateString();
+        }
+        turnContext.StreamingResponse.QueueInformativeUpdateAsync($"Looking up the Weather in {location} for {searchingForDate}");
+
+        // Simulate a delay for the weather service call
         return Task.FromResult(new WeatherForecast
         {
             Date = date,
