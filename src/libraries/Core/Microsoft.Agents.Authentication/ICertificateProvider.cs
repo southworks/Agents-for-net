5aad3e31bcde3d9a2df2d0f8b236c967d3a3d7bb
Peter Hecke <pehecke@microsoft.com>
2025-06-03 11:37:51 -0700

    Fixed build error

--- src/libraries/Core/Microsoft.Agents.Authentication/ICertificateProvider.cs
+++ src/libraries/Core/Microsoft.Agents.Authentication/ICertificateProvider.cs
@@ -4,16 +4,18 @@
 using System.Security.Cryptography.X509Certificates;
 
 namespace Microsoft.Agents.Authentication
-/// <summary>
-/// Defines a provider for retrieving X.509 certificates.
-/// </summary>
-public interface ICertificateProvider
 {
     /// <summary>
-    /// Gets an <see cref="X509Certificate2"/> instance.
+    /// Defines a provider for retrieving X.509 certificates.
     /// </summary>
-    /// <returns>
-    /// An X.509 certificate.
-    /// </returns>
-    X509Certificate2 GetCertificate();
+    public interface ICertificateProvider
+    {
+        /// <summary>
+        /// Gets an <see cref="X509Certificate2"/> instance.
+        /// </summary>
+        /// <returns>
+        /// An X.509 certificate.
+        /// </returns>
+        X509Certificate2 GetCertificate();
+    }
 }
