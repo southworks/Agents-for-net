e2a6b140ad6ef2ac90ee80297ffc1e3fa343d438
Tracy Boehrer <trboehre@microsoft.com>
2025-08-12 10:17:20 -0500

    Fix to allow using a Dialog conditionally in AgentApplication

---
+++ src/samples/DialogAgent/UserProfile.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Agents.Core.Models;
+
+namespace DialogAgent;
+
+/// <summary>
+/// This is our application state. Just a regular serializable .NET class.
+/// </summary>
+public class UserProfile
+{
+    public string? Transport { get; set; }
+
+    public string? Name { get; set; }
+
+    public int? Age { get; set; }
+
+    public Attachment? Picture { get; set; }
+}
