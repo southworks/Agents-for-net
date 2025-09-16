cf071d157680a7c1155373deaa0b72aa09074d77
Tracy Boehrer <trboehre@microsoft.com>
2025-09-16 17:23:03 -0500

    Added ChanneIds for "agents:*"

--- src/libraries/Extensions/Microsoft.Agents.Extensions.A365/A365Extension.cs
+++ src/libraries/Extensions/Microsoft.Agents.Extensions.A365/A365Extension.cs
@@ -14,6 +14,12 @@ namespace Microsoft.Agents.Extensions.A365
     /// </summary>
     public class A365Extension : IAgentExtension
     {
+        public static readonly RouteSelector AgenticMessage = (tc, ct) => Task.FromResult(tc.Activity.Type == ActivityTypes.Message && AgenticAuthorization.IsAgenticRequest(tc));
+        public static readonly RouteSelector AgenticWord = (tc, ct) => Task.FromResult(tc.Activity.Type == ActivityTypes.Message && AgenticAuthorization.IsAgenticRequest(tc) && tc.Activity.ChannelId == Channels.AgentsWord);
+        public static readonly RouteSelector AgenticExcel = (tc, ct) => Task.FromResult(tc.Activity.Type == ActivityTypes.Message && AgenticAuthorization.IsAgenticRequest(tc) && tc.Activity.ChannelId == Channels.AgentsWord);
+        public static readonly RouteSelector AgenticEmail = (tc, ct) => Task.FromResult(tc.Activity.Type == ActivityTypes.Message && AgenticAuthorization.IsAgenticRequest(tc) && tc.Activity.ChannelId == Channels.AgentsEmail);
+        public static readonly RouteSelector AgenticPowerPoint = (tc, ct) => Task.FromResult(tc.Activity.Type == ActivityTypes.Message && AgenticAuthorization.IsAgenticRequest(tc) && tc.Activity.ChannelId == Channels.AgentsPowerPoint);
+
         private readonly AgentApplication _app;
 
         public A365Extension(AgentApplication app)
@@ -41,9 +47,14 @@ namespace Microsoft.Agents.Extensions.A365
         }
         #endregion
 
-        public void OnActivity(string activityType, RouteHandler routeHandler, bool isInvokeRoute = false, ushort rank = 32767, string[] autoSignInHandlers = null, ChannelId channelId = null)
+        public void OnActivity(string activityType, RouteHandler routeHandler, ushort rank = 32767, string[] autoSignInHandlers = null, ChannelId channelId = null)
         {
-            AddRoute(_app, (tc, ct) => Task.FromResult(tc.Activity.IsType(activityType) && channelId == null || tc.Activity.ChannelId == channelId), routeHandler, isInvokeRoute, rank, autoSignInHandlers);
+            AddRoute(_app, (tc, ct) => Task.FromResult(tc.Activity.IsType(activityType) && (channelId == null || tc.Activity.ChannelId == channelId)), routeHandler, false, rank, autoSignInHandlers);
+        }
+
+        public void OnAgenticWord(RouteHandler routeHandler, ushort rank = 32767, string[] autoSignInHandlers = null)
+        {
+            AddRoute(_app, AgenticWord, routeHandler, false, rank, autoSignInHandlers);
         }
     }
 }
