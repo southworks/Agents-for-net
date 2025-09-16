6f44b2460e6efb54d20dff32fb9a43a047d1eac9
Tracy Boehrer <trboehre@microsoft.com>
2025-09-16 16:31:02 -0500

    SerializationInit uses just the assembly level SerialziationInitAttribute

--- src/libraries/Core/Microsoft.Agents.Core/Serialization/SerializationInitAttribute.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Serialization/SerializationInitAttribute.cs
@@ -2,66 +2,12 @@
 // Licensed under the MIT License.
 
 using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Reflection;
 
 namespace Microsoft.Agents.Core.Serialization
 {
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, Inherited = false)]
-    public class SerializationInitAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Assembly, AllowMultiple = true)]
+    public class SerializationInitAttribute(Type type) : Attribute
     {
-        internal static void InitSerialization()
-        {
-            // Register handler for new assembly loads.  This is needed because
-            // C# doesn't load a package until accessed.
-            AppDomain.CurrentDomain.AssemblyLoad += (s, o) => InitAssembly(o.LoadedAssembly);
-
-            // Call serialization init on currently loaded assemblies.
-            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
-            {
-                InitAssembly(assembly);
-            }
-        }
-
-        private static void InitAssembly(Assembly assembly)
-        {
-            foreach (var type in GetLoadOnInitTypes(assembly))
-            {
-                var init = type.GetMethod("Init", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
-                init?.Invoke(assembly, null);
-            }
-        }
-
-        private static IEnumerable<Type> GetLoadOnInitTypes(Assembly assembly)
-        {
-            IList<Type> result = [];
-
-            if (assembly.GetCustomAttributes(typeof(HasSerializationInitAttribute), false).Length == 0)
-            {
-                return result;
-            }
-
-            Type[] types;
-            try
-            {
-                types = assembly.GetTypes();
-            }
-            catch (Exception ex)
-            {
-                System.Diagnostics.Trace.WriteLine($"SerializationInitAttribute.GetLoadOnInitTypes: {ex.Message}");
-                return result;
-            }
-
-            foreach (Type type in types)
-            {
-                if (type.GetCustomAttributes(typeof(SerializationInitAttribute), false).Length > 0)
-                {
-                    result.Add(type);
-                }
-            }
-
-            return result;
-        }
+        public Type InitType = type;
     }
 }
