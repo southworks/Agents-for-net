4b8cbded90d6f4a0cddfbd761c69b03addb265e1
Tracy Boehrer <trboehre@microsoft.com>
2025-09-16 15:29:23 -0500

    Optimized SerializationInit with a new HasSerializationInit assembly attribute

---
+++ src/libraries/Core/Microsoft.Agents.Core/Serialization/HasSerializationInitAttribute.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Agents.Core.Serialization
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class HasSerializationInitAttribute : Attribute
+    {
+    }
+}
--- src/libraries/Core/Microsoft.Agents.Core/Serialization/SerializationInitAttribute.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Serialization/SerializationInitAttribute.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Reflection;
 
 namespace Microsoft.Agents.Core.Serialization
@@ -14,51 +13,34 @@ namespace Microsoft.Agents.Core.Serialization
     {
         internal static void InitSerialization()
         {
-            var go = Stopwatch.StartNew();
-
             // Register handler for new assembly loads.  This is needed because
             // C# doesn't load a package until accessed.
-
-            AppDomain.CurrentDomain.AssemblyLoad += (s, o) => InitAssembly(o.LoadedAssembly, true);
-            System.Diagnostics.Trace.WriteLine($"SerializationInitAttribute.Register Assembly Loader: At {go.ElapsedMilliseconds}ms");
+            AppDomain.CurrentDomain.AssemblyLoad += (s, o) => InitAssembly(o.LoadedAssembly);
 
             // Call serialization init on currently loaded assemblies.
-            var listOfAssembliesForScaning = AppDomain.CurrentDomain.GetAssemblies().Where(w => w.GetTypes().Where(a => Attribute.IsDefined(a, typeof(SerializationInitAttribute))).Any());
-            System.Diagnostics.Trace.WriteLine($"SerializationInitAttribute.FilterAssemblies: At {go.ElapsedMilliseconds}ms - Found: {listOfAssembliesForScaning.Count()}");
-            foreach (var assembly in listOfAssembliesForScaning)
+            foreach (var assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 InitAssembly(assembly);
             }
-            System.Diagnostics.Trace.WriteLine($"SerializationInitAttribute.RunInit: At {go.ElapsedMilliseconds}ms");
-
-            go.Stop();
-            System.Diagnostics.Trace.WriteLine($"SerializationInitAttribute.InitSerialization total: {go.ElapsedMilliseconds}ms");
         }
 
-        private static void InitAssembly(Assembly assembly, bool ScanAssembly = false)
+        private static void InitAssembly(Assembly assembly)
         {
-            var b = Stopwatch.StartNew();
-            if (ScanAssembly && !assembly.GetTypes().Any(a => Attribute.IsDefined(a, typeof(SerializationInitAttribute))))
-            {
-                b.Stop();
-                return;
-            }
-
             foreach (var type in GetLoadOnInitTypes(assembly))
             {
                 var init = type.GetMethod("Init", System.Reflection.BindingFlags.Static | System.Reflection.BindingFlags.Public);
-                System.Diagnostics.Trace.WriteLine($"InitAssembly.GetMethod {assembly.FullName}: At {b.ElapsedMilliseconds}ms");
-
                 init?.Invoke(assembly, null);
-                System.Diagnostics.Trace.WriteLine($"InitAssembly.InvokeInitMethod {assembly.FullName}: AT {b.ElapsedMilliseconds}ms");
             }
-            b.Stop();
-            System.Diagnostics.Trace.WriteLine($"InitAssembly {assembly.FullName}: {b.ElapsedMilliseconds}ms");
         }
 
         private static IEnumerable<Type> GetLoadOnInitTypes(Assembly assembly)
         {
             IList<Type> result = [];
+
+            if (assembly.GetCustomAttributes(typeof(HasSerializationInitAttribute), false).Length == 0)
+            {
+                return result;
+            }
 
             Type[] types;
             try
@@ -78,6 +60,7 @@ namespace Microsoft.Agents.Core.Serialization
                     result.Add(type);
                 }
             }
+
             return result;
         }
     }
