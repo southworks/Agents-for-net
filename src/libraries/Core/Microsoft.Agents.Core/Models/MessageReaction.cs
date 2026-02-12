26497b8a0f81aee3d876ffc0f8885ee2b661f051
Tracy Boehrer <trboehre@microsoft.com>
2026-02-12 13:34:20 -0600

    Added extended Properties to MessageReaction

--- src/libraries/Core/Microsoft.Agents.Core/Models/MessageReaction.cs
+++ src/libraries/Core/Microsoft.Agents.Core/Models/MessageReaction.cs
@@ -3,6 +3,9 @@
 
 #nullable disable
 
+using System.Collections.Generic;
+using System.Text.Json;
+
 namespace Microsoft.Agents.Core.Models
 {
     /// <summary> Message reaction object. </summary>
@@ -19,5 +22,15 @@ namespace Microsoft.Agents.Core.Models
 
         /// <summary> Message reaction types. </summary>
         public string Type { get; set; }
+
+        /// <summary>
+        /// Gets properties that are not otherwise defined by the <see cref="Activity"/> type but that
+        /// might appear in the serialized REST JSON object.
+        /// </summary>
+        /// <value>The extended properties for the object.</value>
+        /// <remarks>With this, properties not represented in the defined type are not dropped when
+        /// the JSON object is deserialized, but are instead stored in this property. Such properties
+        /// will be written to a JSON object when the instance is serialized.</remarks>
+        public IDictionary<string, JsonElement> Properties { get; set; } = new Dictionary<string, JsonElement>();
     }
 }
