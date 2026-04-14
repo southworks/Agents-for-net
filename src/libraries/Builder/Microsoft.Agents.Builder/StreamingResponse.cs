9b82af09d41e1f3a0832b83c97271a25f3075b0f
Tracy Boehrer <trboehre@microsoft.com>
2026-04-14 16:42:27 -0500

    Corrected custom text for StreamingResponse.FinalMessage

--- src/libraries/Builder/Microsoft.Agents.Builder/StreamingResponse.cs
+++ src/libraries/Builder/Microsoft.Agents.Builder/StreamingResponse.cs
@@ -383,7 +383,7 @@ namespace Microsoft.Agents.Builder
             var activity = FinalMessage ?? new Activity();
 
             activity.Type = ActivityTypes.Message;
-            activity.Text = !string.IsNullOrEmpty(Message) ? Message : "No text was streamed";   // Teams won't allow Activity.Text changes or empty text
+            activity.Text ??= !string.IsNullOrEmpty(Message) ? Message : "No text was streamed";   // Teams won't allow Activity.Text changes or empty text
             activity.Entities ??= [];
 
             // make sure the supplied Activity doesn't have a streamInfo already.
