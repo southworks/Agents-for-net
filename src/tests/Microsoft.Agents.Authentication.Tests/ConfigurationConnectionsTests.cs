f6069824ca66bf41bb7beb88ecb40362dd56c873
Tracy Boehrer <trboehre@microsoft.com>
2026-06-12 16:08:12 -0500

    Added missing Microsoft.Agents.Authentication tests

--- src/tests/Microsoft.Agents.Authentication.Tests/ConfigurationConnectionsTests.cs
+++ src/tests/Microsoft.Agents.Authentication.Tests/ConfigurationConnectionsTests.cs
@@ -6,6 +6,7 @@ using Microsoft.Agents.Authentication.Errors;
 using Microsoft.Agents.Authentication.Model;
 using Microsoft.Agents.Authentication.Msal.Model;
 using Microsoft.Agents.Authentication.Msal;
+using Microsoft.Agents.Core.Models;
 using Microsoft.Agents.TestSupport;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -263,6 +264,39 @@ namespace Microsoft.Agents.Auth.Tests
         }
 
         [Fact]
+        public void GetProviderConstructors_ShouldReturnConstructorsForValidAssembly()
+        {
+            var serviceProvider = ServiceProviderBootStrap.CreateServiceProvider(_outputListener, configurationDictionary: null);
+#if !NETFRAMEWORK
+            var assemblyLoader = new AuthModuleLoader(AssemblyLoadContext.Default, serviceProvider.GetService<ILogger<ConfigurationConnections>>());
+#else
+            var assemblyLoader = new AuthModuleLoader(AppDomain.CurrentDomain, serviceProvider.GetService<ILogger<ConfigurationConnections>>());
+#endif
+
+            var constructors = assemblyLoader.GetProviderConstructors("Microsoft.Agents.Authentication.Msal");
+
+            Assert.NotNull(constructors);
+            var list = new List<System.Reflection.ConstructorInfo>(constructors);
+            Assert.True(list.Count > 0);
+        }
+
+        [Fact]
+        public void GetProviderConstructors_ShouldThrowOnNullAssemblyName()
+        {
+            var serviceProvider = ServiceProviderBootStrap.CreateServiceProvider(_outputListener, configurationDictionary: null);
+#if !NETFRAMEWORK
+            var assemblyLoader = new AuthModuleLoader(AssemblyLoadContext.Default, serviceProvider.GetService<ILogger<ConfigurationConnections>>());
+#else
+            var assemblyLoader = new AuthModuleLoader(AppDomain.CurrentDomain, serviceProvider.GetService<ILogger<ConfigurationConnections>>());
+#endif
+
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                foreach (var _ in assemblyLoader.GetProviderConstructors(null)) { }
+            });
+        }
+
+        [Fact]
         public void GetProviderConstructor_ShouldReturnConstructorInfoOnNullType()
         {
             var serviceProvider = ServiceProviderBootStrap.CreateServiceProvider(_outputListener, configurationDictionary: null);
@@ -346,6 +380,155 @@ namespace Microsoft.Agents.Auth.Tests
             var connection = new ConfigurationConnections(providers, mapItems);
             Assert.True(connection.TryGetConnection("ServiceConnection", out var provider));
             Assert.NotNull(provider);
+        }
+
+        [Fact]
+        public void TryGetConnection_ReturnsFalseForNullName()
+        {
+            var sp = new Mock<IServiceProvider>();
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                {
+                    "ServiceConnection", new MsalAuth(sp.Object, new ConnectionSettings()
+                    {
+                        ClientId = "12345",
+                        AuthType = AuthTypes.ClientSecret,
+                    })
+                }
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connection = new ConfigurationConnections(providers, mapItems);
+
+            Assert.False(connection.TryGetConnection(null, out var provider));
+            Assert.Null(provider);
+        }
+
+        [Fact]
+        public void TryGetConnection_ReturnsFalseForNonExistentName()
+        {
+            var sp = new Mock<IServiceProvider>();
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                {
+                    "ServiceConnection", new MsalAuth(sp.Object, new ConnectionSettings()
+                    {
+                        ClientId = "12345",
+                        AuthType = AuthTypes.ClientSecret,
+                    })
+                }
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connection = new ConfigurationConnections(providers, mapItems);
+
+            Assert.False(connection.TryGetConnection("NonExistent", out var provider));
+            Assert.Null(provider);
+        }
+
+        [Fact]
+        public void GetTokenProvider_WithActivity_ReturnsProviderForNonAgenticRole()
+        {
+            var primaryProvider = new Mock<IAccessTokenProvider>();
+            primaryProvider.Setup(p => p.ConnectionSettings).Returns((ImmutableConnectionSettings)null);
+
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                { "ServiceConnection", primaryProvider.Object },
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connections = new ConfigurationConnections(providers, mapItems);
+
+            var activity = new Mock<IActivity>();
+            activity.Setup(a => a.ServiceUrl).Returns("serviceUrl");
+            activity.Setup(a => a.Recipient).Returns(new ChannelAccount { Role = "user" });
+
+            var response = connections.GetTokenProvider(_identity, activity.Object);
+
+            Assert.Same(primaryProvider.Object, response);
+        }
+
+        [Fact]
+        public void GetTokenProvider_WithActivity_UsesAlternateBlueprintForAgenticIdentityRole()
+        {
+            var primaryProvider = new Mock<IAccessTokenProvider>();
+            primaryProvider.Setup(p => p.ConnectionSettings).Returns(new ImmutableConnectionSettings(
+                new TestSettingsWithAlternate("AlternateConnection")));
+
+            var alternateProvider = new Mock<IAccessTokenProvider>();
+            alternateProvider.Setup(p => p.ConnectionSettings).Returns((ImmutableConnectionSettings)null);
+
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                { "ServiceConnection", primaryProvider.Object },
+                { "AlternateConnection", alternateProvider.Object }
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connections = new ConfigurationConnections(providers, mapItems);
+
+            var activity = new Mock<IActivity>();
+            activity.Setup(a => a.ServiceUrl).Returns("serviceUrl");
+            activity.Setup(a => a.Recipient).Returns(new ChannelAccount { Role = RoleTypes.AgenticIdentity });
+
+            var response = connections.GetTokenProvider(_identity, activity.Object);
+
+            Assert.Same(alternateProvider.Object, response);
+        }
+
+        [Fact]
+        public void GetTokenProvider_WithActivity_UsesAlternateBlueprintForAgenticUserRole()
+        {
+            var primaryProvider = new Mock<IAccessTokenProvider>();
+            primaryProvider.Setup(p => p.ConnectionSettings).Returns(new ImmutableConnectionSettings(
+                new TestSettingsWithAlternate("AlternateConnection")));
+
+            var alternateProvider = new Mock<IAccessTokenProvider>();
+            alternateProvider.Setup(p => p.ConnectionSettings).Returns((ImmutableConnectionSettings)null);
+
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                { "ServiceConnection", primaryProvider.Object },
+                { "AlternateConnection", alternateProvider.Object }
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connections = new ConfigurationConnections(providers, mapItems);
+
+            var activity = new Mock<IActivity>();
+            activity.Setup(a => a.ServiceUrl).Returns("serviceUrl");
+            activity.Setup(a => a.Recipient).Returns(new ChannelAccount { Role = RoleTypes.AgenticUser });
+
+            var response = connections.GetTokenProvider(_identity, activity.Object);
+
+            Assert.Same(alternateProvider.Object, response);
+        }
+
+        [Fact]
+        public void GetTokenProvider_WithActivity_DoesNotUseAlternateWhenBlank()
+        {
+            var primaryProvider = new Mock<IAccessTokenProvider>();
+            primaryProvider.Setup(p => p.ConnectionSettings).Returns(new ImmutableConnectionSettings(
+                new TestSettingsWithAlternate(null)));
+
+            var providers = new Dictionary<string, IAccessTokenProvider>
+            {
+                { "ServiceConnection", primaryProvider.Object },
+            };
+            var mapItems = new List<ConnectionMapItem> { new ConnectionMapItem { ServiceUrl = "*", Connection = "ServiceConnection" } };
+            var connections = new ConfigurationConnections(providers, mapItems);
+
+            var activity = new Mock<IActivity>();
+            activity.Setup(a => a.ServiceUrl).Returns("serviceUrl");
+            activity.Setup(a => a.Recipient).Returns(new ChannelAccount { Role = RoleTypes.AgenticIdentity });
+
+            var response = connections.GetTokenProvider(_identity, activity.Object);
+
+            Assert.Same(primaryProvider.Object, response);
+        }
+    }
+
+    internal class TestSettingsWithAlternate : ConnectionSettingsBase
+    {
+        public TestSettingsWithAlternate(string alternateBlueprintConnectionName)
+        {
+            AlternateBlueprintConnectionName = alternateBlueprintConnectionName;
         }
     }
 }
